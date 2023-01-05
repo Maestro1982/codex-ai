9fd197c293a3c07553eddc869a6d42fb1acd544c
Maestro1982 <kris.tote@telenet.be>
2023-01-05 16:05:09 +0100

    update the server url in server.js file

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetch data from the server => bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-ai-vjez.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
